fb405d8bafe4db6138f5024a3df4549e79b1ed09
Samuel Flores <me@samflor.es>
2011-08-11 13:30:41 -0300

    changed creation button text

--- spec/requests/create_project_spec.rb
+++ spec/requests/create_project_spec.rb
@@ -4,14 +4,14 @@ feature 'Create new Project' do
   scenario 'valid attributes' do
     visit '/projects/new'
     fill_in 'Name', :with => 'Super cool project'
-    click_button 'Save'
+    click_button 'Create Project'
     page.should have_content('Super cool project')
     page.should have_content('Project successfully saved')
   end
 
   scenario 'with empty name' do
     visit '/projects/new'
-    click_button 'Save'
+    click_button 'Create Project'
     page.should have_content('Unable to save project')
     page.should have_content('Name can\'t be blank')
   end
